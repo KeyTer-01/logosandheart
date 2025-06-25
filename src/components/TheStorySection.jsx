ee83aed94df71ac5e98d82e4c473600460f3975a
KeyTer-01 <peterbabalola44@gmail.com>
2025-06-25 21:16:34 +0100

    modify story section

--- src/components/TheStorySection.jsx
+++ src/components/TheStorySection.jsx
@@ -16,6 +16,7 @@ const storyItems = [
       "A visual journey into creation, the Word, the beginnings of everything, and Man (spirit, soul, body)",
     bg: "linear(to-br, red.800, red.500)",
     glow: "0 0 10px rgba(239, 68, 68, 0.6)",
+    color: "red.500",
   },
   {
     title: "FAITH HAS BEEN SAVING US",
@@ -23,6 +24,7 @@ const storyItems = [
       "Stories of those who walked by faith, showcasing victories and struggles (Faith without works and Faith by works)",
     bg: "linear(to-br, green.800, green.500)",
     glow: "0 0 10px rgba(72, 187, 120, 0.6)",
+    color: "red.500",
   },
   {
     title: "FAITH WILL SAVE US",
@@ -30,6 +32,7 @@ const storyItems = [
       "A call to live by faith now, no matter the darkness, trusting God's word to carry us in this present time",
     bg: "linear(to-br, yellow.600, yellow.400)",
     glow: "0 0 10px rgba(251, 191, 36, 0.5)",
+    color: "red.500",
   },
 ];
 
@@ -119,7 +122,7 @@ const TheStorySection = () => {
         textAlign="center"
         maxW="3xl"
         mx="auto"
-        mb={12}
+        mb={4}
       >
         <Heading fontSize={{ base: "3xl", md: "5xl" }} mb={4} fontWeight="bold">
           The Story We&apos;re Telling
@@ -133,15 +136,15 @@ const TheStorySection = () => {
       {/* Story Cards */}
       <SimpleGrid
         columns={{ base: 1, md: 3 }}
-        spacing={10}
+        spacing={2}
         pos="relative"
         zIndex={2}
       >
         {storyItems.map((item, index) => (
           <MotionBox
             key={index}
-            bgGradient={item.bg}
-            boxShadow={item.glow}
+            // bgGradient={item.bg}
+            // boxShadow={item.glow}
             p={cardPadding}
             // rounded="2xl"
             textAlign="left"
@@ -154,10 +157,10 @@ const TheStorySection = () => {
               boxShadow: `0 0 50px rgba(255, 255, 255, 0.3)`,
             }}
           >
-            <Text fontSize="xl" fontWeight="bold" mb={3} color="white">
+            <Text fontSize="xl" fontWeight="bold" mb={3} color={"darkRed"}>
               {item.title}
             </Text>
-            <Text fontSize="md" color="whiteAlpha.900">
+            <Text fontSize="md" color="black">
               {item.description}
             </Text>
           </MotionBox>
